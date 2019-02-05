6be7b70269d11de971b7ea55d2e90f582f38dedf
rickschubert <rickschubert@gmx.de>
2019-02-05 13:56:19 +0000

    Need to add plugin to eslint rulese

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     extends: "eslint:recommended",
+    plugins: ["import"],
     rules: {
         "arrow-parens": ["error", "always"],
         "brace-style": ["error", "1tbs", { "allowSingleLine": true }],
